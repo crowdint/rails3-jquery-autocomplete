f78a352c991c282249c15d18101a9a8595afaf69
David Padilla <david@crowdint.com>
2010-09-08 10:40:07 -0500

    Custom form helpers

--- lib/form_helper.rb
+++ lib/form_helper.rb
@@ -5,12 +5,35 @@ module ActionView
       def text_field(object_name, method, options = {})
         original_text_field(object_name, method, rename_autocomplete_option(options))
       end
+
+      # Returns an input tag of the "text" type tailored for accessing a specified attribute (identified by +method+) and
+      # that is populated with jQuery's autocomplete plugin.
+      #
+      # ==== Examples
+      #   autocomplete_field(:post, :title, author_autocomplete_path, :size => 20)
+      #   # => <input type="text" id="post_title" name="post[title]" size="20" value="#{@post.title}"  data-autocomplete="author/autocomplete"/>
+      #
+      def autocomplete_field(object_name, method, source, options ={})
+        options[:autocomplete] = source
+        text_field(object_name, method, options)
+      end
     end
 
     module FormTagHelper
       alias_method :original_text_field_tag, :text_field_tag
       def text_field_tag(name, value = nil, options = {})
         original_text_field_tag(name, value, rename_autocomplete_option(options))
+      end
+
+      # Creates a standard text field that can be populated with jQuery's autocomplete plugin
+      #
+      # ==== Examples
+      #   autocomplete_field_tag 'address', '', address_autocomplete_path, :size => 75
+      #   # => <input id="address" name="address" size="75" type="text" value="" data-autocomplete="address/autocomplete"/>
+      #
+      def autocomplete_field_tag(name, value, source, options ={})
+        options[:autocomplete] = source
+        text_field_tag(name, value, options)
       end
     end
 
--- test/form_helper_test.rb
+++ test/form_helper_test.rb
@@ -1,11 +1,25 @@
 require "test_helper"
 
+class Post
+  attr_accessor :author
+end
+
 class FormHelperTest < ActionView::TestCase
   def test_text_field_tag
     assert_match(/data-autocomplete=\"some\/path\"/, text_field_tag('field_name', '', :autocomplete => 'some/path'))
   end
-  
+
   def test_text_field
-    assert_match(/data-autocomplete=\"some\/path\"/, text_field('field_name', '', :autocomplete => 'some/path'))
+    post = Post.new
+    assert_match(/data-autocomplete=\"some\/path\"/, text_field(:post, :author, :autocomplete => 'some/path'))
+  end
+
+  def test_autocomplete_field_tag
+    assert_match(/data-autocomplete=\"some\/path\"/, autocomplete_field_tag('field_name', '', 'some/path'))
+  end
+
+  def test_autocomplete_field
+    post= Post.new
+    assert_match(/data-autocomplete=\"some\/path\"/, autocomplete_field(:post, :author, 'some/path'))
   end
 end
