9f73519b0291ac509b8e095852b30d221536db99
David Padilla <david@crowdint.com>
2010-09-14 18:24:08 -0700

    Cucumber step to test autocomplete

---
+++ lib/cucumber/autocomplete.rb
@@ -0,0 +1,5 @@
+Given /^I choose "([^"]*)" in the autocomplete list$/ do |text|
+  page.execute_script %Q{ $('input[data-autocomplete]').trigger("keydown") }
+  sleep 1
+  page.execute_script %Q{ $('.ui-menu-item a:contains("#{text}")').trigger("mouseenter").trigger("click"); }
+end
