60e3c203368b4b93a15055d099867363c3b2ed19
David Padilla <david@crowdint.com>
2011-04-04 17:42:19 -0500

    Make sure the autocomplete.js file is deleted before testing the generator

--- test/generators/generator_test.rb
+++ test/generators/generator_test.rb
@@ -4,19 +4,20 @@ require 'generators/autocomplete_generator'
 class AutocompleteGeneratorTest < Test::Unit::TestCase
   def setup
     @destination = File.join('tmp', 'test_app')
-    @source = AutocompleteGenerator.source_root
+    @source      = AutocompleteGenerator.source_root
+    @filename    = File.join(@destination, 'public', 'javascripts', 'autocomplete-rails.js')
+
+    File.unlink(@filename) if File.exists?(@filename)
 
     AutocompleteGenerator.start('', :destination_root => @destination)
   end
 
   def test_install
-    assert File.exists?(
-      File.join(@destination, 'public', 'javascripts', 'autocomplete-rails.js')
-    )
+    assert File.exists?(@filename)
 
     assert_equal(
-      File.read(File.join(@source, 'autocomplete-rails.js')),
-      File.read(File.join(@destination, 'public', 'javascripts', 'autocomplete-rails.js'))
+        File.read(File.join(@source, 'autocomplete-rails.js')),
+        File.read(@filename)
     )
   end
 end
