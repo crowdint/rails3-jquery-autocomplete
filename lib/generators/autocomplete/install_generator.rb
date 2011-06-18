ea0d2fd213cf149dd99fdcb011f789d1d6a45ac1
David Padilla <david@crowdint.com>
2011-06-18 18:16:49 -0500

    Autocomplete generator renamed to Autocomplete::Install generator

---
+++ lib/generators/autocomplete/install_generator.rb
@@ -0,0 +1,14 @@
+require 'rails/generators'
+
+module Autocomplete
+  class InstallGenerator < Rails::Generators::Base
+    def install
+      # Copy the unobtrusive JS file
+      copy_file('autocomplete-rails.js', 'public/javascripts/autocomplete-rails.js')
+    end
+
+    def self.source_root
+      File.join(File.dirname(__FILE__), '..', 'templates')
+    end
+  end
+end
