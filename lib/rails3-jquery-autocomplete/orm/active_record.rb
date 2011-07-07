01e69c4eaef54153e6dde5015b24581c153278e1
David Padilla <david@crowdint.com>
2011-07-07 15:53:39 -0500

    Aesthetic chance on AR Driver

--- lib/rails3-jquery-autocomplete/orm/active_record.rb
+++ lib/rails3-jquery-autocomplete/orm/active_record.rb
@@ -9,14 +9,13 @@ module Rails3JQueryAutocomplete
       end
 
       def get_autocomplete_items(parameters)
-        model      = parameters[:model]
-        term       = parameters[:term]
-        method     = parameters[:method]
-        options    = parameters[:options]
-
-        scopes         = Array(options[:scopes])
-        limit          = get_autocomplete_limit(options)
-        order          = get_autocomplete_order(method, options, model)
+        model   = parameters[:model]
+        term    = parameters[:term]
+        method  = parameters[:method]
+        options = parameters[:options]
+        scopes  = Array(options[:scopes])
+        limit   = get_autocomplete_limit(options)
+        order   = get_autocomplete_order(method, options, model)
 
 
         items = model.scoped
