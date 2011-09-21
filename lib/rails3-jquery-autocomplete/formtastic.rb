7d1d8f5837141f5b75c33b39eaabf127f6b4edce
David Padilla <david@crowdint.com>
2011-09-21 13:20:42 -0700

    Fix Formtastic 1.x compatibility

--- lib/rails3-jquery-autocomplete/formtastic.rb
+++ lib/rails3-jquery-autocomplete/formtastic.rb
@@ -2,19 +2,36 @@
 # Load the formtastic plugin if using Formtastic
 begin
   require 'formtastic'
-  module Formtastic
-    module Inputs
-      class AutocompleteInput
-        include Base
-        include Base::Stringish
 
-        def to_html
-          input_wrapping do
-            label_html <<
+  if Formtastic.constants.include?(:VERSION)
+
+    #
+    # Formtastic 2.0
+    #
+
+
+    module Formtastic
+      module Inputs
+        class AutocompleteInput
+          include Base
+          include Base::Stringish
+
+          def to_html
+            input_wrapping do
+              label_html <<
               builder.autocomplete_field(method, options.delete(:url), input_html_options)
+            end
           end
         end
       end
+    end
+  else
+
+    #
+    # Formtastic 1.x
+    #
+    class Formtastic::FormBuilder < ActionView::Helpers::FormBuilder
+      include Rails3JQueryAutocomplete::FormtasticPlugin
     end
   end
 rescue LoadError
