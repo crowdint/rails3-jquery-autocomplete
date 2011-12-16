37418adeb1a3322d26012b3fb4497780904cb2b1
David Padilla <david@crowdint.com>
2011-12-16 11:52:34 -0600

    Updated integration tests Gemfile.lock

--- integration/features/step_definitions/autocomplete_steps.rb
+++ integration/features/step_definitions/autocomplete_steps.rb
@@ -1,6 +1,6 @@
 Then /^the "([^"]*)" field should contain the "([^"]*)" brand id$/ do |field, name|
   brand = Brand.where(:name => name).first
-  Then %{the "#{field}" field should contain "#{brand.id}"}
+  steps %{Then the "#{field}" field should contain "#{brand.id}"}
 end
 
 And /^I send (.*) to "(.*)"$/ do |key, element|
