3f3982471c28b772f26b3c89c9464bc72bef246f
Tom Chen <developer@tomchentw.com>
2012-05-24 20:47:23 +0800

    Test add hidden_id = true

--- lib/rails3-jquery-autocomplete/simple_form_plugin.rb
+++ lib/rails3-jquery-autocomplete/simple_form_plugin.rb
@@ -3,11 +3,19 @@ module SimpleForm
   module Inputs
     class AutocompleteInput < Base
       def input
-        @builder.autocomplete_field(
+        out = ""
+        out << @builder.autocomplete_field(
           attribute_name,
           options[:url],
           rewrite_autocomplete_option
         )
+        if options[:hidden_id]
+          out << @builder.hidden_field(
+            attribute_name,
+            input_html_options
+          )
+        end
+        out.html_safe
       end
 
     protected
@@ -28,7 +36,12 @@ module SimpleForm
       def rewrite_autocomplete_option
         new_options = {}
         new_options["data-update-elements"] = JSON.generate(options.delete :update_elements) if options[:update_elements]
-        new_options["data-id-element"] = options.delete :id_element if options[:id_element]
+        id_element = options.delete :id_element
+        new_options["data-id-element"] = if id_element == true
+          "##{attribute_name}[type=hidden]"
+        elsif id_element.is_a? String
+          id_element << ", ##{attribute_name}[type=hidden]"
+        end
         input_html_options.merge new_options
       end
     end
